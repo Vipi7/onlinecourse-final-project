ebaf772a59fce5728d7429c68730a89e10f8fff6
Vipin prajapati <121173675+Vipi7@users.noreply.github.com>
2026-02-25 23:43:44 +0530

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,44 +1,20 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Course, Lesson, Question, Choice, Submission
-
-
-def submit(request):
-    questions = Question.objects.all()
-    score = 0
-
-    if request.method == 'POST':
-        for question in questions:
-            selected = request.POST.get(str(question.id))
-            if selected:
-                choice = Choice.objects.get(id=selected)
-                if choice.is_correct:
-                    score += 1
-
-        submission = Submission.objects.create(score=score)
-        return render(
-            request,
-            'onlinecourse/result.html',
-            {
-                'score': score,
-                'questions': questions
-            }
-        )
-
-    return render(request, 'onlinecourse/submit.html', {'questions': questions})
-
-
 def show_exam_result(request, course_id, submission_id):
-    submission = get_object_or_404(Submission, id=submission_id)
-    course = get_object_or_404(Course, id=course_id)
-    questions = Question.objects.filter(course=course)
+    submission = get_object_or_404(Submission, pk=submission_id)
+    selected_ids = submission.choices.values_list('id', flat=True)
 
-    return render(
-        request,
-        'onlinecourse/result.html',
-        {
-            'course': course,
-            'submission': submission,
-            'questions': questions,
-            'score': submission.score
-        }
-    )
+    total_score = 0
+    possible_score = 0
+
+    for question in Question.objects.filter(lesson__course_id=course_id):
+        possible_score += 1
+        if question.is_get_score(selected_ids):
+            total_score += 1
+
+    grade = total_score / possible_score * 100
+
+    context = {
+        'selected_ids': selected_ids,
+        'grade': grade,
+        'possible': possible_score,
+    }
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
