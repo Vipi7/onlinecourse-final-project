78a96d8b514afee55f456d709da41f8d76764c58
Vipin prajapati <121173675+Vipi7@users.noreply.github.com>
2026-02-25 23:42:31 +0530

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -3,7 +3,11 @@ from .models import (
     Course, Lesson, Instructor, Learner,
     Question, Choice, Submission
 )
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'pub_date']
 
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course']
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 1
@@ -20,7 +24,8 @@ class QuestionAdmin(admin.ModelAdmin):
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
-
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Course)
 admin.site.register(Instructor)
 admin.site.register(Learner)
